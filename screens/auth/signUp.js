379c6d6b118c6ba5920181319d1f8468d5111189
jaehyoungyoo <kenshinhm@naver.com>
2019-10-05 21:25:51 +0900

    google login

--- screens/auth/signUp.js
+++ screens/auth/signUp.js
@@ -8,6 +8,7 @@ import {CREATE_ACCOUNT} from "./authQuery.js";
 import AuthButton from "../../components/authButton.js";
 import AuthInput from "../../components/authInput.js";
 import * as Facebook from "expo-facebook";
+import {Google} from "expo";
 
 const View = styled.View`
     justify-content: center;
@@ -22,6 +23,11 @@ const FBContainer = styled.View`
     border-color: ${props => props.theme.lightGreyColor};
     border-style: solid;
 `;
+
+const GoogleContainer = styled.View`
+    margin-top: 20px;
+`;
+
 
 export default ({navigation}) => {
     const fNameInput = useInput("");
@@ -84,11 +90,7 @@ export default ({navigation}) => {
                     `https://graph.facebook.com/me?access_token=${token}&fields=id,last_name,first_name,email`
                 );
                 const {email, first_name, last_name} = await response.json();
-                emailInput.setValue(email);
-                fNameInput.setValue(first_name);
-                lNameInput.setValue(last_name);
-                const [username] = email.split("@");
-                usernameInput.setValue(username);
+                updateFormData(email, first_name, last_name);
                 setLoading(false);
             } else {
                 // type === 'cancel'
@@ -96,6 +98,39 @@ export default ({navigation}) => {
         } catch ({message}) {
             alert(`Facebook Login Error: ${message}`);
         }
+    };
+
+    const googleLogin = async () => {
+        const GOOGLE_ID =
+            "197992685258-6u0isnpjsrs7dc1il65ec5tn7ts7vm73.apps.googleusercontent.com";
+        try {
+            setLoading(true);
+            const result = await Google.logInAsync({
+                iosClientId: GOOGLE_ID,
+                scopes: ["profile", "email"]
+            });
+            if (result.type === "success") {
+                const user = await fetch("https://www.googleapis.com/userinfo/v2/me", {
+                    headers: {Authorization: `Bearer ${result.accessToken}`}
+                });
+                const {email, family_name, given_name} = await user.json();
+                updateFormData(email, given_name, family_name);
+            } else {
+                return {cancelled: true};
+            }
+        } catch (e) {
+            console.log(e);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const updateFormData = (email, firstName, lastName) => {
+        emailInput.setValue(email);
+        fNameInput.setValue(firstName);
+        lNameInput.setValue(lastName);
+        const [username] = email.split("@");
+        usernameInput.setValue(username);
     };
 
     return (
@@ -123,6 +158,13 @@ export default ({navigation}) => {
                                 onPress={fbLogin}
                                 text="Connect Facebook"/>
                 </FBContainer>
+                <GoogleContainer>
+                    <AuthButton
+                        bgColor={"#EE1922"}
+                        loading={false}
+                        onPress={googleLogin}
+                        text="Connect Google"/>
+                </GoogleContainer>
             </View>
         </TouchableWithoutFeedback>
     );
