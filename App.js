208acf711cfbbff835006d736278cf9feef227a5
jaehyoungyoo <kenshinhm@naver.com>
2019-10-20 17:03:53 +0900

    apollo context

--- App.js
+++ App.js
@@ -31,6 +31,12 @@ const App = () => {
             });
             const client = new ApolloClient({
                 cache,
+                request: async (operation) => {
+                    const token = await AsyncStorage.getItem("jwt");
+                    return operation.setContext({
+                        headers: {Authorization: `Bearer ${token}`}
+                    });
+                },
                 ...apolloOptions
             });
             const isLoggedIn = await AsyncStorage.getItem("isLoggedIn");
--- screens/tabs/home.js
+++ screens/tabs/home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import Loader from "../../components/loader.js";
+import {useQuery} from "react-apollo-hooks";
+import {gql} from "apollo-boost";
 
 const View = styled.View`
     justify-content: center;
@@ -8,10 +10,43 @@ const View = styled.View`
     flex: 1;
 `;
 
-const Home = () => (
-    <View>
-        <Loader/>
-    </View>
-);
+const FEED_QUERY = gql`
+    {
+        seeFeed {
+            id
+            location
+            caption
+            user {
+                id
+                avatar
+                username
+            }
+            files {
+                id
+                url
+            }
+            likeCount
+            isLiked
+            comments {
+                id
+                text
+                user {
+                    id
+                    username
+                }
+            }
+            createdAt
+        }
+    }
+`;
+
+const Home = () => {
+    const {loading, data} = useQuery(FEED_QUERY);
+    return (
+        <View>
+            {loading ? <Loader/> : null}
+        </View>
+    );
+};
 
 export default Home;
